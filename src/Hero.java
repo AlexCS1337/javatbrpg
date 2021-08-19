8367b7361ade97b746833176917c4ee4d57d409f
AlexCSilva <alexander.silva@my.jcu.edu.au>
2021-08-19 13:30:23 +1000

    Create Hero class First creating a Hero class for the game that stores the Hero's name, rank and level Also a method to test that it works

---
+++ src/Hero.java
@@ -0,0 +1,27 @@
+public class Hero {
+
+    private String name;
+    private String rank;
+    private int level;
+
+    public Hero(String name, String rank, int level) {
+        this.name = name;
+        this.rank = rank;
+        this.level = level;
+    }
+
+    @Override
+    public String toString() {
+        return "Hero{" +
+            "name'" + name '\'' +
+            ", rank='" + rank + '\'' +
+            "level='" + level + '\'' +
+            '}';
+    }
+
+    //testing the class
+    public static void main(String args[]) throws Exception {
+        Hero hero = new Hero("Hero", "Black Mage", 6;
+        System.out.printline(Hero);
+    }
+}
