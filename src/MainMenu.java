123e70fcf5d47e89a2e4ae4d76cd7a7b35307aa8
AlexCSilva <alexander.silva@my.jcu.edu.au>
2021-09-03 16:57:09 +1000

    Made MainMenu somewhat functional

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -3,18 +3,25 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
 
-        Scanner myObj = new Scanner(System.in);
+        Scanner selection = new Scanner(System.in);
         System.out.println("(N)ew Game\n(C)ontinue\n(Q)uit");
 
-        String playerChoice = myObj.nextLine();
-        while(playerChoice != "Q") {
-            if (playerChoice == "N") {
-                System.out.println("Player choice is:" + playerChoice);
-                //newGame();
-            }
-            else if(playerChoice == "C") {
-                System.out.println("Player choice ise:" + playerChoice);
-                //continueGame();
+        String playerChoice = selection.nextLine();
+        while(!playerChoice.equals("Q")) {
+            System.out.println("(N)ew Game\n(C)ontinue\n(Q)uit");
+            playerChoice = selection.nextLine();
+            switch(playerChoice) {
+                case "N":
+                    System.out.println("player's choice = " + playerChoice);
+                    break;
+                case "C":
+                    System.out.println("player's choice = " + playerChoice);
+                    break;
+                case "Q":
+                    break;
+                default:
+                    System.out.println("Invalid choice! Please make a valid choice.");
+                    break;
             }
         }
         System.out.println("Thanks for playing!");
