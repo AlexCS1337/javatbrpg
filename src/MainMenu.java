e83dfef8fb0aa11ebb497188349d3cf7097f0c43
AlexCSilva <alexander.silva@my.jcu.edu.au>
2021-09-03 17:06:01 +1000

    Made MainMenu class output functional menu MainMenu outputs a functional main menu todo: add new game functionality

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -2,29 +2,28 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
+        String playerChoice;
 
-        Scanner selection = new Scanner(System.in);
-        System.out.println("(N)ew Game\n(C)ontinue\n(Q)uit");
-
-        String playerChoice = selection.nextLine();
-        while(!playerChoice.equals("Q")) {
+        Scanner selection = new Scanner(System.in); 
+  
+        lp : while(true) {
             System.out.println("(N)ew Game\n(C)ontinue\n(Q)uit");
             playerChoice = selection.nextLine();
             switch(playerChoice) {
                 case "N":
+                    //Game newgame = new Game();
                     System.out.println("player's choice = " + playerChoice);
                     break;
                 case "C":
                     System.out.println("player's choice = " + playerChoice);
                     break;
                 case "Q":
-                    break;
+                    System.out.println("Thanks for playing!");
+                    break lp;
                 default:
                     System.out.println("Invalid choice! Please make a valid choice.");
                     break;
             }
-        }
-        System.out.println("Thanks for playing!");
-        System.exit(1);
+        }     
     }
 }
